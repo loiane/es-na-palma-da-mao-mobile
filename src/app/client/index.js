5de55a22d85da62b7a79a0686c87b141377fa71b
hoisel <danielhoiselferraz@gmail.com>
2016-03-11 17:11:34 -0300

    ... broken

--- src/app/client/index.js
+++ src/app/client/index.js
@@ -5,7 +5,7 @@ import angular from 'angular/angular';
 import 'angular-ui-router';
 import 'ui-router-extras';
 import ocLazyLoad from 'oclazyload';
-import routingConfig from './components/config/routing';
+import routingProvider from './components/config/routing';
 import routesConfig from './app.routes';
 import AppController from './app.controller';
 import loggerModule from './components/logger/index';
@@ -15,26 +15,37 @@ import appConfig from './app.config';
 System.import('bootstrap/css/bootstrap.css!').then(() => {
     System.import('src/app/client/app-bootstrap-overrides.css!');
     System.import('src/app/client/app.css!');
-});
+}); 
  
 let layoutModule = angular.module('layout', []);
- 
+  
 const dependencies = [ 
     ocLazyLoad,
-    layoutModule.name,
-    loaderModule.name,
+    layoutModule.name, 
+    loaderModule.name, 
     loggerModule.name, 
     'ui.router',
     'ct.ui.router.extras',
     'ct.ui.router.extras.future'
-];  
+];   
 
-  
+ 
 let app = angular.module('app', dependencies)
-    .run(appConfig)
-    .config(routingConfig)
-    .config(routesConfig)
-    .controller('AppController', AppController);
+                .provider('unicornLauncher', function UnicornLauncherProvider() {
+                    var useTinfoilShielding = false;
+
+                    this.useTinfoilShielding = function(value) {
+                        useTinfoilShielding = !!value;
+                    }; 
+
+                    this.$get = ["apiToken", function unicornLauncherFactory(apiToken) {
+
+                        console.log('tete')
+                    }];
+                 })
+                .config(routesConfig)
+                .controller('AppController', AppController)
+                .run(appConfig);
 
  
 // bootstrap app
