be5852cadbc202ed379099d20c7a8cd6a0fa82b8
hoisel <danielhoiselferraz@gmail.com>
2016-04-28 15:42:10 -0300

    ...

--- src/client/components/app-core/index.js
+++ src/client/components/app-core/index.js
@@ -4,14 +4,14 @@ import angular from 'angular/angular';
 import 'angular-ui-router';
 import 'ui-router-extras';
 import ocLazyLoad from 'oclazyload';
-import RouterHelperProvider from './config/router-helper.provider';
-import loggerModule from './logger/index';
-import loaderModule from './loader/index';
-import routesConfig from './app.routes';
-import AppController from './app.controller';
-import appRun from './app.run';
-import updateTitleDirective from '../directives/update-title.directive'; //eslint-disable-line
-                                                                         // no-unused-vars
+import RouterHelperProvider from './config/router-helper.provider.js';
+import loggerModule from './logger/index.js';
+import loaderModule from './loader/index.js';
+import routesConfig from './app.routes.js';
+import AppController from './app.controller.js';
+import appRun from './app.run.js';
+import updateTitleDirective from '../directives/update-title.directive.js'; //eslint-disable-line
+// no-unused-vars
 //eslint-disable-line
 // no-unused-vars
 let layoutModule = angular.module( 'layout', [] );
