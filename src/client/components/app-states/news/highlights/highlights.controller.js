37351ef03c5704f3d8592f34b033dd16e86a80d2
hoisel <danielhoiselferraz@gmail.com>
2016-06-22 11:53:43 -0300

    bugFix(news): Corrige exibicão da primeira notícia em destaque

--- src/client/components/app-states/news/highlights/highlights.controller.js
+++ src/client/components/app-states/news/highlights/highlights.controller.js
@@ -29,7 +29,7 @@ class HighlightsController {
      * @returns {*|{}}
      */
     get firstNews() {
-        return this.highlights[ 0 ] || {};
+        return this.highlights[ 0 ];
     }
 
     /**
