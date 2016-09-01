feb3430a61bb88e5679fbaa28ed18c5302df0a93
hoisel <danielhoiselferraz@gmail.com>
2016-09-01 08:44:58 -0300

    refact(shared/menu): Adiciona detran/shared como dependência

--- src/components/shared/menu/index.ts
+++ src/components/shared/menu/index.ts
@@ -1,5 +1,6 @@
 import menuComponent from './menu.component';
+import detranShared from '../../detran/shared/index';
 
-export default angular.module( 'layout.menu', [] )
+export default angular.module( 'layout.menu', [ detranShared.name ] )
                       .directive( 'menu', menuComponent );
 
