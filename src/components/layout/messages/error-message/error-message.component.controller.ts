f4c64fcba0471bd31926fcce15a4ca60ea2c0b8e
hoisel <danielhoiselferraz@gmail.com>
2016-09-05 15:54:40 -0300

    feat(error-message): Suporte a erros 400 e 429

--- src/components/layout/messages/error-message/error-message.component.controller.ts
+++ src/components/layout/messages/error-message/error-message.component.controller.ts
@@ -11,4 +11,15 @@ export class ErrorMessageController {
     constructor() {
          this.unhandledErrorMessage = this.unhandledErrorMessage || 'Erro inesperado';
     }
+
+
+    /**
+     * Indica se é um server error (500-599)
+     * 
+     * @readonly
+     * @type {boolean}
+     */
+    public get isServerError(): boolean {
+        return /5\d{2}/.test( this.error.status.toString() );
+    }
 }
