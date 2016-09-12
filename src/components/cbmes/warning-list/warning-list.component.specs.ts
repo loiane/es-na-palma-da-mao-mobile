128e004b31b73763fe43157e156f9f0ebd314329
hoisel <danielhoiselferraz@gmail.com>
2016-09-12 16:34:09 -0300

    refact(cmbes): Amplia cobertura de tests

--- src/components/cbmes/warning-list/warning-list.component.specs.ts
+++ src/components/cbmes/warning-list/warning-list.component.specs.ts
@@ -21,14 +21,15 @@ let expect = chai.expect;
 describe( 'cmbes/warning-list', () => {
 
     let sandbox: Sinon.SinonSandbox;
-    beforeEach( () => sandbox = sinon.sandbox.create() );
-    afterEach( () => sandbox.restore() );
+    beforeEach(() => sandbox = sinon.sandbox.create() );
+    afterEach(() => sandbox.restore() );
 
     describe( 'Controller', () => {
         let controller: WarningListController;
         let cbmesApiService: CbmesApiService;
         let onIonicBeforeEnterEvent;
-        let $window;
+        let $window: any;
+        let $scope: any;
 
         // mocka data
         let warnings: Warning[] =
@@ -69,25 +70,25 @@ describe( 'cmbes/warning-list', () => {
                 }
             }];
 
-        beforeEach( () => {
-            $window = sandbox.stub();
-            let $scope = <any>{
+        beforeEach(() => {
+            $window =  <any>{ open() {} };
+            $scope = <any>{
                 $on: ( event, callback ) => {
                     if ( event === '$ionicView.beforeEnter' ) {
                         onIonicBeforeEnterEvent = callback;
                     }
                 }
             };
-            cbmesApiService = <CbmesApiService>{ getLastWarnings() {} };
+            cbmesApiService = <CbmesApiService>{ getLastWarnings() { } };
             controller = new WarningListController( $scope, $window, cbmesApiService );
-        } );
+        });
 
         describe( 'on instantiation', () => {
 
             it( 'should have a empty warning list', () => {
                 expect( controller.warnings ).to.be.undefined;
                 expect( controller.warnings ).to.be.empty;
-            } );
+            });
 
             it( 'should activate on $ionicView.beforeEnter event', () => {
                 let activate = sandbox.stub( controller, 'activate' ); // replace original activate
@@ -96,43 +97,74 @@ describe( 'cmbes/warning-list', () => {
                 onIonicBeforeEnterEvent();
 
                 expect( activate.called ).to.be.true;
-            } );
-        } );
+            });
+        });
 
         describe( 'activate()', () => {
 
             let getWarnings: Sinon.SinonSpy;
 
-            beforeEach( () => {
+            beforeEach(() => {
                 getWarnings = sandbox.spy( controller, 'getWarnings' );
 
                 sandbox.stub( cbmesApiService, 'getLastWarnings' ).returnsPromise().resolves( warnings );
 
                 controller.activate();
-            } );
+            });
 
             it( 'should call getLastWarnings()', () => {
                 expect( getWarnings.calledOnce ).to.be.true;
-            } );
-        } );
+            });
+        });
 
 
         describe( 'getWarnings()', () => {
 
-           let getLastWarnings: Sinon.SinonStub;
+            let getLastWarnings: Sinon.SinonStub;
 
-            beforeEach( () => {
+            beforeEach(() => {
                 getLastWarnings = sandbox.stub( cbmesApiService, 'getLastWarnings' );
                 getLastWarnings.returnsPromise().resolves( warnings );
 
                 controller.getWarnings();
-            } );
+            });
 
             it( 'should fill warnings list', () => {
                 expect( controller.warnings ).to.deep.equal( warnings );
-            } );
-        } );
-    } );
+            });
+        });
+
+        describe( 'openLocation()', () => {
+
+            let lat: number;
+            let lng: number;
+            let label: string;
+            let $windowOpen: Sinon.SinonStub;
+
+            beforeEach(() => {
+                lat = 2343434;
+                lng = 2342342;
+                label = 'Es';
+                $windowOpen = sinon.stub( $window, 'open' );
+            });
+
+            it( 'should open google maps on Web', () => {
+                $scope.isAndroid = false;
+
+                controller.openLocation( lat, lng, label );
+
+                expect( $windowOpen.calledWith( `maps://?q=${lat},${lng}`, '_system' ) ).to.be.true;
+            });
+
+            it( 'should open google maps on Android device', () => {
+                $scope.isAndroid = true;
+
+                controller.openLocation( lat, lng, label );
+
+                expect( $windowOpen.calledWith( `geo:0,0?q=${lat},${lng}(${encodeURI( label )})`, '_system' ) ).to.be.true;
+            });
+        });
+    });
 
     describe( 'Component', () => {
         // test the component/directive itself
@@ -140,23 +172,23 @@ describe( 'cmbes/warning-list', () => {
 
         it( 'should use the right controller', () => {
             expect( component.controller ).to.equal( WarningListController );
-        } );
+        });
 
         it( 'should use the right template', () => {
             expect( component.template ).to.equal( WarningListTemplate );
-        } );
+        });
 
         it( 'should use controllerAs', () => {
             expect( component ).to.have.property( 'controllerAs' );
-        } );
+        });
 
         it( 'should use controllerAs "vm"', () => {
             expect( component.controllerAs ).to.equal( 'vm' );
-        } );
+        });
 
         it( 'should use bindToController: true', () => {
             expect( component ).to.have.property( 'bindToController' );
             expect( component.bindToController ).to.equal( true );
-        } );
-    } );
-} );
+        });
+    });
+});
