a9d838a12f0b31267e5f75c293fd54a7ba5cf69f
hoisel <danielhoiselferraz@gmail.com>
2016-09-12 17:38:42 -0300

    refact(dashboard): Adiciona tests para controller

--- src/components/dashboard/dashboard.component.specs.ts
+++ src/components/dashboard/dashboard.component.specs.ts
@@ -1,10 +1,10 @@
-﻿ /*
- eslint
- no-undef: 0,
- dot-notation: 0,
- angular/di: 0,
- no-unused-expressions: 0
- */
+﻿/*
+eslint
+no-undef: 0,
+dot-notation: 0,
+angular/di: 0,
+no-unused-expressions: 0
+*/
 
 import { DashBoardController } from './dashboard.component.controller';
 import DashBoardComponent from './dashboard.component';
@@ -20,8 +20,64 @@ let expect = chai.expect;
 describe( 'Dashboard', () => {
 
     let sandbox: Sinon.SinonSandbox;
-    beforeEach( () => sandbox = sinon.sandbox.create() );
-    afterEach( () => sandbox.restore() );
+    beforeEach(() => sandbox = sinon.sandbox.create() );
+    afterEach(() => sandbox.restore() );
+
+    describe( 'Controller', () => {
+        let controller: DashBoardController;
+        let onIonicBeforeEnterEvent;
+        let $state: angular.ui.IStateService;
+
+        beforeEach(() => {
+            let $scope = <any>{
+                $on: ( event, callback ) => {
+                    if ( event === '$ionicView.beforeEnter' ) {
+                        onIonicBeforeEnterEvent = callback;
+                    }
+                }
+            };
+
+            $state = <angular.ui.IStateService><any>{ go: () => { } };
+
+            controller = new DashBoardController( $scope, $state );
+        });
+
+        describe( 'on instantiation', () => {
+            it( 'should activate on $ionicView.beforeEnter event', () => {
+                let activate = sandbox.stub( controller, 'activate' ); // replace original activate
+
+                // simulates ionic before event trigger
+                onIonicBeforeEnterEvent();
+
+                expect( activate.called ).to.be.true;
+            });
+        });
+
+        describe( 'activate()', () => {
+            it( 'should apply "espm-header-tab" class to all "ion-header-bar" elements', () => {
+                let domElement = { addClass: sinon.spy() };
+                let querySelectorAll = sandbox.stub( document, 'querySelectorAll' );
+                sandbox.stub( angular, 'element' ).returns( domElement );
+
+                controller.activate();
+
+                expect( querySelectorAll.calledWith( 'ion-header-bar' ) ).to.be.true;
+                expect( domElement.addClass.calledWith( 'espm-header-tabs' ) ).to.be.true;
+            });
+        });
+
+
+        describe( 'navigateTo( newState )', () => {
+            it( 'should redirect user to "newState"', () => {
+                let go = sandbox.stub( $state, 'go' );
+                let newState = 'newState';
+
+                controller.navigateTo( newState );
+
+                expect( go.calledWith( newState ) ).to.be.true;
+            });
+        });
+    });
 
     describe( 'Component', () => {
         // test the component/directive itself
@@ -29,23 +85,23 @@ describe( 'Dashboard', () => {
 
         it( 'should use the right controller', () => {
             expect( component.controller ).to.equal( DashBoardController );
-        } );
+        });
 
         it( 'should use the right template', () => {
             expect( component.template ).to.equal( DashBoardTemplate );
-        } );
+        });
 
         it( 'should use controllerAs', () => {
             expect( component ).to.have.property( 'controllerAs' );
-        } );
+        });
 
         it( 'should use controllerAs "vm"', () => {
             expect( component.controllerAs ).to.equal( 'vm' );
-        } );
+        });
 
         it( 'should use bindToController: true', () => {
             expect( component ).to.have.property( 'bindToController' );
             expect( component.bindToController ).to.equal( true );
-        } );
-    } );
-} );
+        });
+    });
+});
