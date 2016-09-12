7c55398ab04e44dd2bd0d8b7f032e108928d5cf7
hoisel <danielhoiselferraz@gmail.com>
2016-09-12 17:38:04 -0300

    refact(dashboard):  Chama 'activate' via evento '$ionicView.beforeEnter'

--- src/components/dashboard/dashboard.component.controller.ts
+++ src/components/dashboard/dashboard.component.controller.ts
@@ -1,14 +1,16 @@
+import { IScope } from 'angular';
+
 export class DashBoardController {
 
-    public static $inject: string[] = [ '$state' ];
+    public static $inject: string[] = [ '$scope', '$state' ];
 
     /**
      * Creates an instance of DashBoardController.
      * 
      * @param {angular.ui.IStateService} $state
      */
-    constructor( private $state: angular.ui.IStateService ) {
-        this.activate();
+    constructor( private $scope: IScope, private $state: angular.ui.IStateService ) {
+        this.$scope.$on( '$ionicView.beforeEnter', () => this.activate() );
     }
 
     /**
