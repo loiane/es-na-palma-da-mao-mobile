e988cf2aae0d880893a0163df5d36e573e532ee2
hoisel <danielhoiselferraz@gmail.com>
2016-09-22 11:40:10 -0300

    refact(app): Ajusta importação da fonte roboto

--- src/components/app/index.ts
+++ src/components/app/index.ts
@@ -10,7 +10,7 @@ import 'ui-router-extras';
 import 'oclazyload';
 
 import 'font-awesome';
-import 'roboto/css/roboto/roboto-fontface.css';
+import 'roboto';
 
 // shared
 import shared from '../shared/index';
