915c3e01e1e87ddafd8729de64e0286b57eaf772
hoisel <danielhoiselferraz@gmail.com>
2016-10-11 18:59:16 -0300

    feat(shared/transitions):  Adiciona método que limpa cache de transições

--- src/components/shared/transition.service.ts
+++ src/components/shared/transition.service.ts
@@ -30,45 +30,92 @@ export class TransitionService {
         this.$mdSidenav( 'left' ).close();
     }
 
+
+    /**
+     * 
+     * 
+     * @param {string} stateName
+     * 
+     * @memberOf TransitionService
+     */
     public changeRootState( stateName: string ): void {
         this.changeState( stateName, {}, {}, true );
     }
 
+
+    /**
+     * 
+     * 
+     * @param {string} stateName
+     * @param {string} direction
+     * 
+     * @memberOf TransitionService
+     */
     public changeTab( stateName: string, direction: string ): void {
         let options = { type: 'slide', direction: direction };
         this.changeState( stateName, {}, options, false, true );
     }
 
+    /**
+     * 
+     * 
+     * @param {string} stateName
+     * @param {*} [routeParameters={}]
+     * @param {*} [options={}]
+     * @param {boolean} [isRoot=false]
+     * @param {boolean} [isTabs=false]
+     * @param {boolean} [reload=false]
+     * 
+     * @memberOf TransitionService
+     */
     public changeState( stateName: string, routeParameters: any = {}, options: any = {}, isRoot: boolean = false, isTabs: boolean = false, reload: boolean = false ): void {
         // this.$timeout(() => {
-            this.closeSideNav();
+        this.closeSideNav();
 
-            let defaultOptions: any = { type: 'fade' };
-            angular.extend( defaultOptions, options );
+        let defaultOptions: any = { type: 'fade' };
+        angular.extend( defaultOptions, options );
 
-            if ( isRoot ) {
-                this.$ionicHistory.nextViewOptions( {
-                    disableBack: true,
-                    historyRoot: true
-                });
+        if ( isRoot ) {
+            this.$ionicHistory.nextViewOptions( {
+                disableBack: true,
+                historyRoot: true
+            });
+        }
+
+        if ( isTabs ) {
+            if ( this.$rootScope.isAndroid ) {
+                defaultOptions.fixedPixelsTop = 93;
+            } else if ( this.$rootScope.isIOS ) {
+                defaultOptions.fixedPixelsBottom = 48;
             }
+        }
 
-            if ( isTabs ) {
-                if ( this.$rootScope.isAndroid ) {
-                    defaultOptions.fixedPixelsTop = 93;
-                } else if ( this.$rootScope.isIOS ) {
-                    defaultOptions.fixedPixelsBottom = 48;
-                }
+        if ( this.$ionicNativeTransitions ) {
+            this.$ionicNativeTransitions.stateGo( stateName, routeParameters, { reload: reload }, defaultOptions );
+        } else {
+            if ( this.$ionicHistory.currentStateName() !== stateName ) {
+                this.$state.go( stateName, routeParameters );
             }
-
-            if ( this.$ionicNativeTransitions ) {
-                this.$ionicNativeTransitions.stateGo( stateName, routeParameters, { reload: reload }, defaultOptions );
-            } else {
-                if ( this.$ionicHistory.currentStateName() !== stateName ) {
-                    this.$state.go( stateName, routeParameters );
-                }
-            }
+        }
 
         // }, ( this.$rootScope.isAndroid === false ? 300 : 0 ) );
+    }
+
+    /**
+     * 
+     * 
+     * 
+     * @memberOf TransitionService
+     */
+    public clearCache(): Promise<void> {
+        // limpa view caches do ionic
+        // ref: http://stackoverflow.com/questions/29593018/ionic-there-is-a-way-to-delete-the-cache-in-controller-method
+        return new Promise(( resolve ) => {
+            this.$timeout(() => {
+                this.$ionicHistory.clearCache();
+                this.$ionicHistory.clearHistory();
+                resolve();
+            }, 100 );
+        });
     }
 }
